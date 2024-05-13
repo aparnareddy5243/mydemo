ebdc0aa7ca3b4b84bec934daea847436bfbe8114
appureddyt <apputmrreddy@gmail.com>
2024-05-13 22:40:55 +0100

    urkhj

--- main.tf
+++ main.tf
@@ -35,11 +35,11 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   }
 }
 
-resource "azurerm_app_service" "webapp" {
+resource "azurerm_windows_web_app" "webapp" {
   name                = "Webapp5243"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  service_plan_id     = azurerm_app_service_plan.appserviceplan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
