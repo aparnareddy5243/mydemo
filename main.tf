8979bed84544a2e7ffa69a117a3fa3468091eb4b
appureddyt <apputmrreddy@gmail.com>
2024-05-14 09:37:37 +0100

    ggg

--- main.tf
+++ main.tf
@@ -25,23 +25,32 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "webapp5243plan"
-  location            = "East US"
-  resource_group_name = "ap_rg"
+  name                = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
+  # Correct SKU configuration using a block
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"  # B for Basic tier
+    size = "B1"     # SKU size
   }
+
+  # Specify the kind of app service plan, e.g., for Linux or Windows
+  kind = "Linux"  # Use "Linux" or "Windows"
+  reserved = true  # This must be set to true for Linux
 }
 
-resource "azurerm_windows_web_app" "webapp" {
+
+resource "azurerm_Linux_web_app" "webapp" {
   name                = "Webapp5243"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_app_service_plan.appserviceplan.id
+  http_only           = true
 
   site_config {
     dotnet_framework_version = "v4.0"
   }
 }
+
+
